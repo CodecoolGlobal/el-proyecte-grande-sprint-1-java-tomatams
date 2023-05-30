98fc0f79419c7323e0c644b7108d066c40d2cc5b
csege <cseloteigergely@gmail.com>
2023-05-30 15:01:47 +0200

    Add Storage class which stores recipes, return with the list, can add new item to the list.

---
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/service/Storage.java
@@ -0,0 +1,20 @@
+package com.codecool.elproyectegrandesprint.javatomatams.service;
+
+import com.codecool.elproyectegrandesprint.javatomatams.model.RecipeDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Storage {
+    private final List<RecipeDTO> recipes = new ArrayList<>();
+
+    public void addRecipe(RecipeDTO newRecipe) {
+        recipes.add(newRecipe);
+    }
+
+    public List<RecipeDTO> getRecipes() {
+        return recipes;
+    }
+}
