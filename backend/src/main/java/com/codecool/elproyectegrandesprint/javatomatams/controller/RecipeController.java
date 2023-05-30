c6d81d1a20da7fc6830977ce1ddc998716acf0e0
csege <cseloteigergely@gmail.com>
2023-05-30 15:02:33 +0200

    Add Controller with get request handle.

---
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/controller/RecipeController.java
@@ -0,0 +1,25 @@
+package com.codecool.elproyectegrandesprint.javatomatams.controller;
+
+import com.codecool.elproyectegrandesprint.javatomatams.model.RecipeDTO;
+import com.codecool.elproyectegrandesprint.javatomatams.service.Storage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recipes")
+public class RecipeController {
+
+    private final Storage storage;
+
+    public RecipeController(Storage storage) {
+        this.storage = storage;
+    }
+
+    @GetMapping(value = "all")
+    public List<RecipeDTO> getAllRecipes() {
+        return storage.getRecipes();
+    }
+}
