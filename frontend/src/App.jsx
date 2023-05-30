7852f9ca1ae7e5f18f98001521230cd59f6cfa3c
pjung86 <peter.jung.codecool@gmail.com>
2023-05-30 15:35:03 +0200

    add header and layout component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
+import Layout from "./Pages/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Layout/>
     </div>
   );
 }
---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1>First day of Pet Project</h1>
+        <button>Yessss</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
