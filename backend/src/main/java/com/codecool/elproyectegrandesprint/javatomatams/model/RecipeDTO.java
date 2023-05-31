e8aabf9a69bd9842ffad1766bf721eecbfd2c79c
tomatams <tomatams@gmail.com>
2023-05-31 10:58:51 +0200

    change record to class

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/model/RecipeDTO.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/model/RecipeDTO.java
@@ -3,5 +3,21 @@ package com.codecool.elproyectegrandesprint.javatomatams.model;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record RecipeDTO(UUID id, String name, String preparation, LocalDateTime creationDate) {}
+public class RecipeDTO {
+    private final UUID ID;
+    private final String name;
+    private final String preparation;
+    private final LocalDateTime creationDate;
+
+    public RecipeDTO(String name, String preparation, LocalDateTime creationDate) {
+        this.ID = UUID.randomUUID();
+        this.name = name;
+        this.preparation = preparation;
+        this.creationDate = creationDate;
+    }
+
+    public boolean isThisID(UUID id){
+        return ID.equals(id);
+    }
+}
 
