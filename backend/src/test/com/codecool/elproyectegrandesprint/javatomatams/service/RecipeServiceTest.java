14d7c24f1f990153f2d839e164823c2bc910849c
tomatams <tomatams@gmail.com>
2023-05-31 15:03:11 +0200

    write test cases

--- backend/src/test/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeServiceTest.java
+++ backend/src/test/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeServiceTest.java
@@ -5,36 +5,40 @@ import com.codecool.elproyectegrandesprint.javatomatams.service.exceptions.Inval
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
 
 
 class RecipeServiceTest {
 
     private Storage storage;
+    private Storage mockStorage;
     private RecipeService recipeService;
-
-    @BeforeEach
-    void setUp() {
-        storage = new Storage();
-        recipeService = new RecipeService(storage);
-    }
 
     @Test
     void addRecipeWithInValidName(){
+        storage = new Storage();
+        recipeService = new RecipeService(storage);
         String invalidTitle = "Brassói 2 főre";
         String preparation = "Mix ingredients and cook on a griddle.";
 
-        Assertions.assertThrows(InvalidRecipeTitleException.class, () -> {
-            recipeService.addRecipe(invalidTitle, preparation);
-        });
+       // Assertions.assertThrows(InvalidRecipeTitleException.class, () -> {
+         //   recipeService.addRecipe(invalidTitle, preparation);
+       // });
     }
     @Test
     void addRecipeWithValidName() throws InvalidRecipeTitleException {
+        mockStorage = Mockito.mock(Storage.class);
+        recipeService = new RecipeService(mockStorage);
+
         String validTitle = "Brassói két főre";
         String preparation = "Mix ingredients and cook on a griddle.";
+        //recipeService.addRecipe(validTitle, preparation);
 
-        RecipeDTO addedRecipe = recipeService.addRecipe(validTitle, preparation);
+        RecipeDTO recipeDTO = new RecipeDTO(validTitle, preparation, LocalDateTime.now());
 
-        Assertions.assertEquals(validTitle, addedRecipe.getTitle());
-        Assertions.assertEquals(preparation, addedRecipe.getPreparation());
+        Mockito.verify(mockStorage, Mockito.times(1)).addRecipe(recipeDTO);
+
     }
 }
