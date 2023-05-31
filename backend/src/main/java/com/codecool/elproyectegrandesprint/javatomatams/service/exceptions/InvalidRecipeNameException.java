f42d78bf75480609212d593385f497abcd545697
tomatams <tomatams@gmail.com>
2023-05-31 11:16:34 +0200

    add exception to throw fail

---
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/service/exceptions/InvalidRecipeNameException.java
@@ -0,0 +1,7 @@
+package com.codecool.elproyectegrandesprint.javatomatams.service.exceptions;
+
+public class InvalidRecipeNameException extends Exception {
+    public InvalidRecipeNameException(String s) {
+        super(s);
+    }
+}
