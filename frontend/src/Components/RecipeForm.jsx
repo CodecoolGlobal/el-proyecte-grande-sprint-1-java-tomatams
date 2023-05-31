f84b295ccd339b24ca3b21ae24db9c82b64cbcf7
pjung86 <peter.jung.codecool@gmail.com>
2023-05-31 15:04:22 +0200

    add flex to recipe form

--- frontend/src/Components/RecipeForm.jsx
+++ frontend/src/Components/RecipeForm.jsx
@@ -1,29 +1,42 @@
-const RecipeForm = ({recipe, onSave, disabled, onCancel}) =>{
-  
+const RecipeForm = ({ recipe, onSave, disabled, onCancel }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const entries = [...formData.entries()];
 
     const recipe = entries.reduce((acc, entry) => {
-      const[k, v] = entry;
+      const [k, v] = entry;
       acc[k] = v;
       return acc;
     }, {});
     return onSave(recipe);
-  }
+  };
 
   return (
     <form onSubmit={onSubmit}>
-      <div className="formContainer">
-      <label htmlFor="title">Recipe title</label>
-      <input id="title" name="title" defaultValue={recipe ? recipe.title : null}></input>
-      <label htmlFor="preparation">Preparation</label>
-      <textarea id="preparation" name="preparation">{recipe ? recipe.preparation : null}</textarea>
-      <button type="submit" disabled={disabled}>{recipe ? "Update recipe" : "Create recipe"}</button>
-      <button onClick={onCancel}>Cancel</button>
+      <div className="form-container">
+        <div className="form-fields">
+          <label htmlFor="title">Recipe title</label>
+          <input
+            id="title"
+            name="title"
+            defaultValue={recipe ? recipe.title : null}
+          ></input>
+        </div>
+        <div className="form-fields">
+          <label htmlFor="preparation">Preparation</label>
+          <textarea id="preparation" name="preparation">
+            {recipe ? recipe.preparation : null}
+          </textarea>
+        </div>
+        <div className="form-field-buttons">
+          <button type="submit" disabled={disabled}>
+            {recipe ? "Update recipe" : "Create recipe"}
+          </button>
+          <button onClick={onCancel}>Cancel</button>
+        </div>
       </div>
     </form>
-  )
-}
+  );
+};
 export default RecipeForm;
