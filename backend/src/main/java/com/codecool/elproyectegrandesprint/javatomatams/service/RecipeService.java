5b3f891f3cb71e0474e125c4be0b95a63ca0c126
tomatams <tomatams@gmail.com>
2023-05-31 10:38:03 +0200

    validate given name from fronted

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeService.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeService.java
@@ -1,9 +1,9 @@
 package com.codecool.elproyectegrandesprint.javatomatams.service;
 
 import com.codecool.elproyectegrandesprint.javatomatams.model.RecipeDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 @Service
 public class RecipeService {
@@ -18,8 +18,24 @@ public class RecipeService {
         return storage.getAllRecipes();
     }
 
-    public void addRecipe(RecipeDTO newRecipe) {
-        storage.addRecipe(newRecipe);
+    public RecipeDTO addRecipe(String name, String preparation) {
+        if (isValidName(name)){
+            RecipeDTO newRecipe = new RecipeDTO(name, preparation, LocalDateTime.now());
+            storage.addRecipe(newRecipe);
+            return newRecipe;
+        } else {
+            return null;
+        }
+    }
+
+    private boolean isValidName(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
     }
 
 
