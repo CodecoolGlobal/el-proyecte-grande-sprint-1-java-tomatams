0f2b64c721197027d485d43b9510024ff6f679ca
tomatams <tomatams@gmail.com>
2023-05-31 13:43:46 +0200

    write testcases for RecipeService

---
+++ backend/src/test/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeServiceTest.java
@@ -0,0 +1,44 @@
+package com.codecool.elproyectegrandesprint.javatomatams.service;
+
+import com.codecool.elproyectegrandesprint.javatomatams.model.RecipeDTO;
+import com.codecool.elproyectegrandesprint.javatomatams.service.exceptions.InvalidRecipeTitleException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeServiceTest {
+
+    private Storage storage;
+    private RecipeService recipeService;
+
+    @BeforeEach
+    void setUp() {
+        storage = new Storage();
+        recipeService = new RecipeService(storage);
+    }
+
+    @Test
+    void addRecipeWithInValidName(){
+        String invalidTitle = "Brassói 2 főre";
+        String preparation = "Mix ingredients and cook on a griddle.";
+
+        Assertions.assertThrows(InvalidRecipeTitleException.class, () -> {
+            recipeService.addRecipe(invalidTitle, preparation);
+        });
+    }
+    @Test
+    void addRecipeWithValidName() throws InvalidRecipeTitleException {
+        String validTitle = "Brassói két főre";
+        String preparation = "Mix ingredients and cook on a griddle.";
+
+        RecipeDTO addedRecipe = recipeService.addRecipe(validTitle, preparation);
+
+        Assertions.assertEquals(validTitle, addedRecipe.getTitle());
+        Assertions.assertEquals(preparation, addedRecipe.getPreparation());
+    }
+}
