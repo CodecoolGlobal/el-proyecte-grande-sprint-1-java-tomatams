b6b9941fe4c6b2af076012091253306066771d57
tomatams <tomatams@gmail.com>
2023-05-31 15:02:44 +0200

    validate the titlename

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/model/RecipeDTO.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/model/RecipeDTO.java
@@ -1,5 +1,7 @@
 package com.codecool.elproyectegrandesprint.javatomatams.model;
 
+import com.codecool.elproyectegrandesprint.javatomatams.service.exceptions.InvalidRecipeTitleException;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -9,7 +11,8 @@ public class RecipeDTO {
     private final String preparation;
     private final LocalDateTime creationDate;
 
-    public RecipeDTO(String title, String preparation, LocalDateTime creationDate) {
+    public RecipeDTO(String title, String preparation, LocalDateTime creationDate) throws InvalidRecipeTitleException {
+        titleValidator(title);
         this.ID = UUID.randomUUID();
         this.title = title;
         this.preparation = preparation;
@@ -34,6 +37,21 @@ public class RecipeDTO {
 
     public boolean isThisID(UUID id){
         return ID.equals(id);
+    }
+
+    private void titleValidator (String title) throws InvalidRecipeTitleException {
+        if(!isValidTitle(title)){
+            throw new InvalidRecipeTitleException("Can't add this recipe, because not valid title:" + title);
+        }
+    }
+    public boolean isValidTitle(String title) {
+        for (int i = 0; i < title.length(); i++) {
+            char c = title.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
