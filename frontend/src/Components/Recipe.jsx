f7780de4d64b687d1fb77ff65344f83fa512b693
csekea <113106666+csekea@users.noreply.github.com>
2023-06-01 15:34:04 +0200

    format recipe preparation text

--- frontend/src/Components/Recipe.jsx
+++ frontend/src/Components/Recipe.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Recipe = ({recipe}) => {
+const Recipe = ({ recipe }) => {
 
   return (
-      <div>
-        <h2>{recipe.title}</h2>
-        <p>{recipe.preparation}</p>
-        <p>{recipe.creationDate}</p>
-      </div>
+    <div>
+      <h2>{recipe.title}</h2>
+      <p className="recipe-preparation-text">{recipe.preparation}</p>
+      <p>{recipe.creationDate}</p>
+    </div>
 
   )
 }
