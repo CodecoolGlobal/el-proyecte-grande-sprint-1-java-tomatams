c265fd9716340df3192deb97d610ec313c39fa9c
tomatams <tomatams@gmail.com>
2023-06-01 07:53:43 +0200

    delete unused test

--- backend/src/test/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeServiceTest.java
+++ backend/src/test/com/codecool/elproyectegrandesprint/javatomatams/service/RecipeServiceTest.java
@@ -16,29 +16,4 @@ class RecipeServiceTest {
     private Storage mockStorage;
     private RecipeService recipeService;
 
-    @Test
-    void addRecipeWithInValidName(){
-        storage = new Storage();
-        recipeService = new RecipeService(storage);
-        String invalidTitle = "Brassói 2 főre";
-        String preparation = "Mix ingredients and cook on a griddle.";
-
-       // Assertions.assertThrows(InvalidRecipeTitleException.class, () -> {
-         //   recipeService.addRecipe(invalidTitle, preparation);
-       // });
-    }
-    @Test
-    void addRecipeWithValidName() throws InvalidRecipeTitleException {
-        mockStorage = Mockito.mock(Storage.class);
-        recipeService = new RecipeService(mockStorage);
-
-        String validTitle = "Brassói két főre";
-        String preparation = "Mix ingredients and cook on a griddle.";
-        //recipeService.addRecipe(validTitle, preparation);
-
-        RecipeDTO recipeDTO = new RecipeDTO(validTitle, preparation, LocalDateTime.now());
-
-        Mockito.verify(mockStorage, Mockito.times(1)).addRecipe(recipeDTO);
-
-    }
 }
