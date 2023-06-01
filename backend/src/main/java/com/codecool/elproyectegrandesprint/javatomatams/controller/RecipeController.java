b9316af1a6bba3c832fe4d6aeefc162b3304bb05
tomatams <tomatams@gmail.com>
2023-06-01 09:03:08 +0200

    add random recipes

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/controller/RecipeController.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/controller/RecipeController.java
@@ -3,9 +3,13 @@ package com.codecool.elproyectegrandesprint.javatomatams.controller;
 import com.codecool.elproyectegrandesprint.javatomatams.model.RecipeDTO;
 import com.codecool.elproyectegrandesprint.javatomatams.service.RecipeService;
 import com.codecool.elproyectegrandesprint.javatomatams.service.exceptions.InvalidRecipeTitleException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -17,6 +21,7 @@ public class RecipeController {
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+        //populateRecipes();
     }
 
     @GetMapping(value = "all")
@@ -42,6 +47,20 @@ public class RecipeController {
             return ResponseEntity.ok(recipeService.getRecipeByID(id));
         } catch (NoSuchElementException e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(value = "/add-recipes-from-json")
+    public ResponseEntity<List<RecipeDTO>> addRecipesFromJson() {
+        try {
+            InputStream inputStream = new ClassPathResource("recipes.json").getInputStream();
+            List<NewRecipeDTO> recipes = objectMapper.readValue(inputStream, new TypeReference<List<NewRecipeDTO>>() {});
+            List<RecipeDTO> addedRecipes = recipeService.addRecipes(recipes);
+            return ResponseEntity.ok(addedRecipes);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (InvalidRecipeTitleException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 }
