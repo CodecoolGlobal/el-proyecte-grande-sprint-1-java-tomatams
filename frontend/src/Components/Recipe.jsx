dbdb00a566a99888ddb6b7ed44372edc758c4dc3
tomatams <tomatams@gmail.com>
2023-06-14 11:58:28 +0200

    add img tag to recipe

--- frontend/src/Components/Recipe.jsx
+++ frontend/src/Components/Recipe.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Recipe = ({ recipe }) => {
-  console.log("Recipe", typeof recipe.ingredientDTOS);
+
   return (
     <div>
-      <h2>{recipe.title}</h2>
+      <img src={recipe.image} className="recipe-image" alt="Recipe" />
       <div>
         {recipe.ingredientDTOS.map((ingredient, index) => (
           <div key={ingredient.id}>
@@ -17,7 +17,6 @@ const Recipe = ({ recipe }) => {
       <p className="recipe-preparation-text">{recipe.preparation}</p>
       <p>{recipe.creationDate}</p>
     </div>
-
   )
 }
 
