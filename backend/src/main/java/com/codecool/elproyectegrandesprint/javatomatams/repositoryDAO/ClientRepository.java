1dac291d6092838ab57a9088664102e140d7302d
tomatams <tomatams@gmail.com>
2023-06-27 11:32:49 +0200

    add method to find client

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/repositoryDAO/ClientRepository.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/repositoryDAO/ClientRepository.java
@@ -4,6 +4,11 @@ import com.codecool.elproyectegrandesprint.javatomatams.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ClientRepository extends JpaRepository<Client,Long> {
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+
+    Client findClientByName(String name);
+
 }
