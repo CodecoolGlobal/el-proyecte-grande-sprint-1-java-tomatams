18ecb5639e9fb2eb20aa5d771b44880161b3a1e5
tomatams <tomatams@gmail.com>
2023-06-27 10:59:29 +0200

    add inmemory users to project

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/security/WebSecurityConfig.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/security/WebSecurityConfig.java
@@ -1,19 +1,37 @@
 package com.codecool.elproyectegrandesprint.javatomatams.security;
 
+import com.codecool.elproyectegrandesprint.javatomatams.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
 
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles(String.valueOf(Role.ADMIN))
+                .build();
 
+        UserDetails test = User.withUsername("test")
+                .password(passwordEncoder().encode("test"))
+                .roles(String.valueOf(Role.USER))
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, test);
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
