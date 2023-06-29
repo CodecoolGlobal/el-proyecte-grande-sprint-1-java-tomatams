a41df3ecf848ea79727dcb4bf56255712249b4e2
tomatams <tomatams@gmail.com>
2023-06-29 11:07:17 +0200

    update buildering user in loadusername method

--- backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/security/ClientDetailsService.java
+++ backend/src/main/java/com/codecool/elproyectegrandesprint/javatomatams/security/ClientDetailsService.java
@@ -3,6 +3,7 @@ package com.codecool.elproyectegrandesprint.javatomatams.security;
 import com.codecool.elproyectegrandesprint.javatomatams.model.Client;
 import com.codecool.elproyectegrandesprint.javatomatams.repositoryDAO.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ClientDetailsService implements UserDetailsService {
 
-    private ClientRepository clientRepository;
+    private final ClientRepository clientRepository;
 
     @Autowired
     public ClientDetailsService(ClientRepository clientRepository) {
@@ -23,6 +24,9 @@ public class ClientDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Client client = clientRepository.findClientByName(username);
-        return null;
+        return User.withUsername(client.getName())
+                .password(client.getPassword())
+                .roles(String.valueOf(client.getRole()))
+                .build();
     }
 }
